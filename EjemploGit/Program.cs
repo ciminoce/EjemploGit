9e6ebcb44cc10f874583bbc00d83548f36550f89
ciminoce <ciminoce@yahoo.com.ar>
2024-02-19 20:04:11 -0300

    Agregar archivos de proyecto.

---
+++ EjemploGit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EjemploGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
